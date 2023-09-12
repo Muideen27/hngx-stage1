5ef575647abc8bf143455cc28a82a5844e724ccd
Muideen27 <ilorimuideen0000@gmail.com>
2023-09-12 22:54:09 +0100

    App.js modified @utc

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000; // Choose a suitable port
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
   // Retrieve query parameters
-  const slackName = req.query.slack_name;
+  const slack_name = req.query.slack_name;
   const track = req.query.track;
   const githubFileUrl = req.query.github_file_url;
   const githubRepoUrl = req.query.github_repo_url;
@@ -14,15 +14,18 @@ app.get('/', (req, res) => {
 
   // Get the current UTC time with a +/- 2 minute window
   const currentDateTime = new Date();
-  currentDateTime.setMinutes(currentDateTime.getMinutes() - 2);
-  const utcTime = currentDateTime.toISOString().replace('T', ' Time:').replace(/\.\d{3}Z$/, '');
+  // currentDateTime.setMinutes(currentDateTime.getMinutes() - 2);
+  // const utcTime = currentDateTime.toISOString().replace('T', ' Time:').replace(/\.\d{3}Z$/, '');
+  const utcTime = currentDateTime.setMinutes(currentDateTime.getMinutes() - 2)
+  const utcTimenew = currentDateTime.toISOString().split('.')[0] + 'Z';
+
 
   // Prepare the JSON response
   const responseData = {
-    slack_name: 'Muideen Ilori',
+    slack_name: slack_name,
     current_day: currentDay,
-    utc_time: utcTime,
-    track: 'Backend',
+    utc_time: utcTimenew,
+    track: track,
     github_file_url: 'https://github.com/Muideen27/hngx-stage1.git/app.js',
     github_repo_url: 'https://github.com/Muideen27/hngx-stage1.git',
     status_code: 200,
